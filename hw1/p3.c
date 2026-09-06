499dd81274c2b1cc82681089ba349b6e02833eaa
hoseony <hoseon.alex.yu@gmail.com>
2026-09-06 10:48:37 -0400

    moving to vm

--- hw1/p3.c
+++ hw1/p3.c
@@ -11,31 +11,48 @@
 int main(int argc, char ** argv) {
     if (argc == 1) {
         fprintf(stderr, "usage: kit [-o outfile] infile1 [...infile2....]\n");
-        fprintf(stderr, "       kit [-o outfile]");
+        fprintf(stderr, "       kit [-o outfile]\n");
         return -1;
     }
 
     /* check for the output flag "-o" */
     int opt; 
-    int idx = 0;
+    int flag_idx = 0; // keeps track of the index of output file
+
     char *output = NULL;
+ 
+    for (int i = 0; i < argc -1; i++) {
+        printf("argv[%d]: %s\n",i, argv[i]);
+    }
+    putchar('\n');
 
     while ((opt = getopt(argc, argv, "o:")) != -1) {
         switch (opt) {
         case 'o':
+         
+            for (int i = 0; i < argc -1; i++) {
+                printf("argv[%d]: %s\n",i, argv[i]);
+            }
+            putchar('\n');
+
             output = optarg;
-            idx = optind;
+            flag_idx = optind - 1;
+            printf("flag_dx: %d\n", flag_idx);
             break;
+
         default: 
             fprintf(stderr, "usage: kit [-o outfile] infile1 [...infile2....]\n");
-            fprintf(stderr, "       kit [-o outfile]");
+            fprintf(stderr, "       kit [-o outfile]\n");
             exit(EXIT_FAILURE);
        }
+
+        putchar('\n');
     }
 
-    printf("output file: %s | index: %d\n", output, idx);
 
-    
+    printf("output file: %s | index: %d\n", output, flag_idx);
+
+
     /* decide which file descriptor to use for output */
     int out_fd = 1; // if no flag stdout
 
@@ -51,9 +68,28 @@ int main(int argc, char ** argv) {
     char buf[BUFFSIZE];
     
     /* find next file from command-line */
-    while(idx < argc) {
-        printf("arg[%d]: %s\n", idx, argv[idx]);
+
+    // It should skeep indicies of things it previously read.
+    // Specifically, -o and its arg
+
+
+    for (int i = 0; i < argc -1; i++) {
+        printf("argv[%d]: %s\n",i, argv[i]);
+    }
+    putchar('\n');
+
+
+
+    int idx = 1;
+
+    while(1) {
+        if (idx == flag_idx || idx == flag_idx - 1 || idx == argc - 1) {
+            continue;
+        }
+
+        printf("argv[%d]: %s\n", idx, argv[idx]);
         idx++;
+
     }
 
     return 0;
